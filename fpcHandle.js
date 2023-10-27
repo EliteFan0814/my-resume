76733ed084317abfea3389c73cadfc7db5d5e2d1
EliteFan <fan552426811@outlook.com>
2023-10-27 22:27:52 +0800

    20231027

--- fpcHandle.js
+++ fpcHandle.js
@@ -5,6 +5,13 @@ var mySwiper = new Swiper('#swiper1', {
     el: '.swiper-pagination1',
   },
 })
+var mySwiper = new Swiper('#swiper2', {
+  autoplay: true, //可选选项，自动滑动
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination1',
+  },
+})
 
 //如果你初始化时没有定义Swiper实例，后面也可以通过Swiper的HTML元素来获取该实例
 // new Swiper('.swiper')
